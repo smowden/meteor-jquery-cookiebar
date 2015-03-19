cfd26db8cfbd120d5ffc7325f6d7680c863b940a
smowden <github@codesue.la>
2015-03-19 05:05:51 +0100

    version constraint

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'smowden:jquery-cookiebar',
-  version: '0.0.1',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Drop in of the jquery.cookiebar plugin to comply with EU cookie law',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
@@ -13,7 +13,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.2');
   api.use("jquery", "client");
-  api.use("benjaminrh:jquery-cookie", "client");
+  api.use("benjaminrh:jquery-cookie@0.1.0", "client");
   api.addFiles('jquery.cookiebar/jquery.cookiebar.js', "client");
   api.addFiles('jquery.cookiebar/jquery.cookiebar.css', "client");
 });
